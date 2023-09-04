88a347b1875e1d9fc313165590b87ebaefc21d81
Kenneth <kenneth.mburu@student.moringaschool.com>
2023-09-04 20:39:59 +0300

    animal list

--- app.py
+++ app.py
@@ -17,11 +17,29 @@ animals = [
         "type": "Female",
         "weight": 230
     },
-        {
+    {
         "name": "Snow Fox",
         "image": "https://images.unsplash.com/photo-1474511320723-9a56873867b5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1172&q=80",
         "type": "Female",
         "weight": 23
+    },
+    {
+        "name": "Snow Fox",
+        "image": "https://cdn.pixabay.com/photo/2021/03/03/14/55/rhino-6065480_1280.jpg",
+        "type": "Female",
+        "weight": 23
+    },
+        {
+        "name": "Elephant",
+        "image": "https://images.unsplash.com/photo-1603483080228-04f2313d9f10?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1965&q=80",
+        "type": "Female",
+        "weight": 23
+    },
+            {
+        "name": "Leopart",
+        "image": "https://plus.unsplash.com/premium_photo-1664304400278-8152cba3da20?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1188&q=80",
+        "type": "Female",
+        "weight": 70
     }
 ]
 
